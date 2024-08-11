518a10789e70bca2b7fffcb9880a4116f7b4216a
LuisPedroFarias <161033603+LuisPedroFarias@users.noreply.github.com>
2024-08-11 02:23:33 -0300

    Add files via upload

---
+++ Aprendendo_em_casa_part1.java
@@ -0,0 +1,18 @@
+package com.mycompany.atividadesss;
+import java.util.Locale;
+import java.util.Date;
+
+
+public class Aprendendo_em_casa_part1 {
+
+    public static void main(String[] args) {
+        Locale localizacao = Locale.getDefault();
+        Date relogio = new Date();
+        
+        
+        System.out.println("O sistema operacional está na lingua: "+ localizacao);
+        
+        System.out.println("Data e hora: "+ relogio);
+        
+    }
+}
